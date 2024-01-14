3c92ab58c8720c12fbce8d05ab8292e3b5acf14f
pokhym <ng121@purdue.edu>
2024-01-14 16:30:22 -0500

    add testcase for missing genre type and mp

--- test_handler_mangadex.py
+++ test_handler_mangadex.py
@@ -3,6 +3,43 @@ import logging
 from handler_mangadex import *
 from constants import *
 
+import multiprocessing as mp
+
+def download(tid):
+  if tid == 0:
+    mh = HandlerMangaDex(0, SOURCE_MANGADEX)
+    mh.reset_for_next_title()
+    
+    mh.init_for_title("test/", "https://mangadex.org/title/879af0bb-ce30-47e4-a74e-cd1ce874c6e3/isekai-nonbiri-nouka")
+    
+    mh.extract_metadata()
+
+    mh.start_driver()
+    mh.init_for_chapter("216", "https://mangadex.org/chapter/d1d38099-b7a4-459b-ad4c-729bd74c55f9")
+    mh.extract_chapter_images()
+    mh.reset_for_next_chapter()
+    mh.terminate_driver()
+  elif tid == 1:
+    mh = HandlerMangaDex(1, SOURCE_MANGADEX)
+    mh.reset_for_next_title()
+    
+    mh.init_for_title("test/", "https://mangadex.org/title/ffe69cc2-3f9e-4eab-a7f7-c963cea9ec25/hitoribocchi-no-isekai-kouryaku")
+    
+    mh.extract_metadata()
+
+    mh.start_driver()
+    mh.init_for_chapter("216", "https://mangadex.org/chapter/207f85d5-06cd-4e01-ac3a-64c009bfad93")
+    mh.extract_chapter_images()
+    mh.reset_for_next_chapter()
+    mh.terminate_driver()
+  else:
+    assert(0)
+
+def test_mp():
+  
+  p = mp.Pool(2)
+  p.map(download, [i for i in range(2)])
+
 def test_chapter_download():
   mh = HandlerMangaDex(0, SOURCE_MANGADEX)
   mh.reset_for_next_title()
@@ -10,7 +47,7 @@ def test_chapter_download():
   mh.init_for_title("test/", "https://mangadex.org/title/879af0bb-ce30-47e4-a74e-cd1ce874c6e3/isekai-nonbiri-nouka")
   
   mh.extract_metadata()
-  
+
   mh.start_driver()
   mh.init_for_chapter("216", "https://mangadex.org/chapter/d1d38099-b7a4-459b-ad4c-729bd74c55f9")
   mh.extract_chapter_images()
@@ -53,6 +90,16 @@ def test_title_metadata2():
 
   mh.metadata.dump()
 
+def test_title_metadata3():
+  mh = HandlerMangaDex(0, SOURCE_MANGADEX)
+  mh.reset_for_next_title()
+
+  mh.init_for_title("test/", "https://mangadex.org/title/027df837-7a15-4893-9dc3-e2ae11b94717/2d5bcdb0-e2a3-4352-9043-2ab218bee11b")
+
+  mh.extract_metadata()
+
+  mh.metadata.dump()
+
 def test_language_selection():
   mh = HandlerMangaDex(0, SOURCE_MANGADEX)
   mh.reset_for_next_title()
@@ -88,4 +135,4 @@ if __name__ == "__main__":
   # add the handler to the root logger
   logging.getLogger().addHandler(console)
   
-  test_chapter_download()
+  test_title_metadata3()
